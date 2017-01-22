4f176af7d5afc4203bfc4a3cbe2d25cf5d23a159
Jeff Patton <jeffrey@patton-tech.com>
2017-01-21 21:49:24 -0600

    New cmdlet test-protocol

--- SchannelManagement/Protocols/Protocols.psm1
+++ SchannelManagement/Protocols/Protocols.psm1
@@ -365,3 +365,83 @@ function Get-Protocol
 		}
 	}
 }
+
+function Test-Protocol
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory=$false, Position=1)]
+		[switch]$Client,
+		[Parameter(Mandatory=$false, Position=2)]
+		[string]$ComputerName
+	)
+
+	DynamicParam
+	{
+		$ParameterName = 'Protocol';
+		$RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary;
+		$AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute];
+		$ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute;
+		$ParameterAttribute.Mandatory = $true;
+		$ParameterAttribute.Position = 0;
+		$AttributeCollection.Add($ParameterAttribute);
+		$arrSet = $Protocols.Keys;
+		$ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet);
+		$AttributeCollection.Add($ValidateSetAttribute);
+		$RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection);
+		$RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter);
+		return $RuntimeParameterDictionary;
+	}
+
+	begin
+	{
+		$Protocol = $PSBoundParameters[$ParameterName];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			Write-Verbose "Protocol     : $($Protocol)";
+			Write-Verbose "Client       : $($Client)";
+			Write-Verbose "ComputerName : $($ComputerName)";
+
+			if ($Client)
+			{
+				$RegPath = "$($ProtocolsPath)\$($Protocol)\Client";
+			}
+			else
+			{
+				$RegPath = "$($ProtocolsPath)\$($Protocol)\Server";
+			}
+
+			Write-Verbose "RegPath      : $($RegPath)";
+
+			if ($ComputerName)
+			{
+				$Result = Get-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ComputerName $ComputerName;
+			}
+			else
+			{
+				$Result = Get-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath;
+			}
+
+			if ($Result.ReturnValue -ne 5)
+			{
+				return $true;
+			}
+			else
+			{
+				return $false;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+}
