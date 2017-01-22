d20e67f15a0a2f9cd6650dd9b47169b207aead5b
Jeff Patton <jeffrey@patton-tech.com>
2017-01-22 00:55:01 -0600

    Updating Ciphers

--- SchannelManagement/Ciphers/Ciphers.psm1
+++ SchannelManagement/Ciphers/Ciphers.psm1
@@ -1,6 +1,15 @@
-<#
-	My Function
-#>
-function Get-Function {
+$CiphersPath = "SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers";
+$Ciphers = @{};
+$Ciphers.Add('AES 128/128',$CiphersPath);
+$Ciphers.Add('AES 256/256',$CiphersPath);
+$Ciphers.Add('DES 56/56',$CiphersPath);
+$Ciphers.Add('NULL',$CiphersPath);
+$Ciphers.Add('RC2 128/128',$CiphersPath);
+$Ciphers.Add('RC2 40/128',$CiphersPath);
+$Ciphers.Add('RC2 56/128',$CiphersPath);
+$Ciphers.Add('RC4 128/128',$CiphersPath);
+$Ciphers.Add('RC4 40/128',$CiphersPath);
+$Ciphers.Add('RC4 56/128',$CiphersPath);
+$Ciphers.Add('RC4 64/128',$CiphersPath);
+$Ciphers.Add('Triple DES 168/168',$CiphersPath);
 
-}
