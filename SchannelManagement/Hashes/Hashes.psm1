714dcc5a1270d9237fec5e9767910a24299afd56
Jeff Patton <jeffrey@patton-tech.com>
2017-01-24 22:33:23 -0600

    Added new/remove cmdlet

--- SchannelManagement/Hashes/Hashes.psm1
+++ SchannelManagement/Hashes/Hashes.psm1
@@ -1,4 +1,134 @@
-$HashesPath = "SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Hashes"
+$HashesPath = "SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Hashes";
 $Hashes = @{};
 $Hashes.Add('MD5',$HashesPath);
 $Hashes.Add('SHA',$HashesPath);
+
+function New-Hash
+{
+	[CmdletBinding()]
+	param
+	(
+	)
+
+	DynamicParam
+	{
+
+		$DynamicParameters = @(
+			@{
+				Name = 'Hash'
+				Type = [array]
+				Position = 0
+				Mandatory = $true
+				ValidateSet = $Hashes.Keys
+			},
+			@{
+				Name = 'ComputerName'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
+	}
+
+	begin
+	{
+		$Hash = $PSBoundParameters['Hash'];
+		$ComputerName = $PSBoundParameters['ComputerName'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			Write-Verbose "Hash       : $($Hash)";
+			Write-Verbose "ComputerName : $($ComputerName)";
+
+			$RegPath = "$($Hashes[$Hash])\$($Hash)";
+
+			Write-Verbose "RegPath      : $($RegPath)";
+
+			if ($ComputerName)
+			{
+				New-RegistryKey -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ComputerName $ComputerName;
+			}
+			else
+			{
+				New-RegistryKey -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+}
+
+function Remove-Hash
+{
+	[CmdletBinding()]
+	param
+	(
+	)
+
+	DynamicParam
+	{
+
+		$DynamicParameters = @(
+			@{
+				Name = 'Hash'
+				Type = [array]
+				Position = 0
+				Mandatory = $true
+				ValidateSet = $Hashes.Keys
+			},
+			@{
+				Name = 'ComputerName'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
+	}
+
+	begin
+	{
+		$Hash = $PSBoundParameters['Hash'];
+		$ComputerName = $PSBoundParameters['ComputerName'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			Write-Verbose "Hash       : $($Hash)";
+			Write-Verbose "ComputerName : $($ComputerName)";
+
+			$RegPath = "$($Hashes[$Hash])\$($Hash)";
+
+			Write-Verbose "RegPath      : $($RegPath)";
+
+			if ($ComputerName)
+			{
+				Remove-RegistryKey -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ComputerName $ComputerName;
+			}
+			else
+			{
+				Remove-RegistryKey -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+}
