9e121e0aa0097a706083445758ef4774cf3866d1
Jeff Patton <jeffrey@patton-tech.com>
2017-01-24 22:35:59 -0600

    Added enable/disable cmldet

--- SchannelManagement/Hashes/Hashes.psm1
+++ SchannelManagement/Hashes/Hashes.psm1
@@ -132,3 +132,134 @@ function Remove-Hash
 		}
 	}
 }
+
+function Enable-Hash
+{
+	[CmdletBinding()]
+	param
+	(
+	)
+
+	DynamicParam
+	{
+
+		$DynamicParameters = @(
+			@{
+				Name = 'Hash'
+				Type = [array]
+				Position = 0
+				Mandatory = $true
+				ValidateSet = $Hashes.Keys
+			},
+			@{
+				Name = 'ComputerName'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
+	}
+
+	begin
+	{
+		$Hash = $PSBoundParameters['Hash'];
+		$ComputerName = $PSBoundParameters['ComputerName'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			Write-Verbose "Hash       : $($Hash)";
+			Write-Verbose "ComputerName : $($ComputerName)";
+
+			$RegPath = "$($Hashes[$Hash])\$($Hash)";
+
+			Write-Verbose "RegPath      : $($RegPath)";
+
+			if ($ComputerName)
+			{
+				Set-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ValueName 'Enabled' -ValueType DWORD -Value 4294967295 -ComputerName $ComputerName;
+			}
+			else
+			{
+				Set-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ValueName 'Enabled' -ValueType DWORD -Value 4294967295;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+}
+
+
+function Disable-Hash
+{
+	[CmdletBinding()]
+	param
+	(
+	)
+
+	DynamicParam
+	{
+
+		$DynamicParameters = @(
+			@{
+				Name = 'Hash'
+				Type = [array]
+				Position = 0
+				Mandatory = $true
+				ValidateSet = $Hashes.Keys
+			},
+			@{
+				Name = 'ComputerName'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
+	}
+
+	begin
+	{
+		$Hash = $PSBoundParameters['Hash'];
+		$ComputerName = $PSBoundParameters['ComputerName'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			Write-Verbose "Hash       : $($Hash)";
+			Write-Verbose "ComputerName : $($ComputerName)";
+
+			$RegPath = "$($Hashes[$Hash])\$($Hash)";
+
+			Write-Verbose "RegPath      : $($RegPath)";
+
+			if ($ComputerName)
+			{
+				Set-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ValueName 'Enabled' -ValueType DWORD -Value 0 -ComputerName $ComputerName;
+			}
+			else
+			{
+				Set-RegistryValue -Hive HKEY_LOCAL_MACHINE -SubKeyName $RegPath -ValueName 'Enabled' -ValueType DWORD -Value 0;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+}
